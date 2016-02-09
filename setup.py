f99791480f9f5807fc9755668e5e79f719bddb54
Lope <Lope922@gmail.com>
2016-02-09 16:29:18 -0600

    asciiart initial commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setup import setup 
+
+	setup(name= 'AsciiArt', 
+	version = '0.1dev',
+	description = 'Ascii "art" from images',
+	author= 'Lope',
+	packages = ['asciiart']
+	intsall_requires=['pillow'])

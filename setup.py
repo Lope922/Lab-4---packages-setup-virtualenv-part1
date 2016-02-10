a5671367c8aa55eede4635be16b6e7a686ad97f5
Lope <Lope922@gmail.com>
2016-02-09 21:49:26 -0600

    found typeo in setup file and fixed

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from setup import setup 
+from setuptools import setup 
 
-	setup(name= 'AsciiArt', 
-	version = '0.1dev',
-	description = 'Ascii "art" from images',
-	author= 'Lope',
-	packages = ['asciiart']
-	intsall_requires=['pillow'])
+setup(name='AsciiArt', 
+    version = '0.1.dev',
+    description = 'Ascii "art" from images',
+    author= 'Lope',
+    packages = ['asciiart'],
+    intsall_requires=['pillow'])
